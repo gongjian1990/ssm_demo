082d22cd2fbb7a082897ab93083228613ca72594
gongjian <Administrator@XC3EPWADMX0V4HX>
2018-11-09 15:25:55 +0800

    提交IoUtils，apache StringUtils 工具类用法

--- src/main/java/com/gj/dao/BookDao.java
+++ src/main/java/com/gj/dao/BookDao.java
@@ -1,8 +1,17 @@
 package com.gj.dao;
 
 import com.gj.entity.Book;
+import org.apache.ibatis.annotations.Param;
 
 public interface BookDao {
 
     Book getBook(int id);
+
+    /**
+     * 此处@Param 必填
+     * @param sql
+     * @return
+     */
+    Book getBook1(@Param(value="sql")String sql);
+
 }
